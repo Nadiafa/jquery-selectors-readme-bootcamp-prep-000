9b04faa5010bb74f52b5659c3d14fc21143037d8
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-31 07:02:06 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,3 +4,27 @@
 
 
 // );
+
+
+
+
+
+  1) Selectors paragraphSelector() should select all paragraphs:
+     ReferenceError: paragraphSelector is not defined
+      at Context.<anonymous> (test/index-test.js:3:5)
+
+  2) Selectors lastImageSelector() should return the last image:
+     ReferenceError: lastImageSelector is not defined
+      at Context.<anonymous> (test/index-test.js:8:5)
+
+  3) Selectors ninjaBabySelector() should return the image of the ninja baby:
+     ReferenceError: ninjaBabySelector is not defined
+      at Context.<anonymous> (test/index-test.js:13:5)
+
+  4) Selectors divSelector() should return two divs:
+     ReferenceError: divSelector is not defined
+      at Context.<anonymous> (test/index-test.js:18:5)
+
+  5) Selectors firstListItem() should return the first list item in the unordered list:
+     ReferenceError: firstListItem is not defined
+      at Context.<anonymous> (test/index-test.js:23:5)
